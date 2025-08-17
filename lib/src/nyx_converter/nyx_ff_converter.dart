831847af08de3a51271797bed280470c4d061131
Alexandre Sousa Dantas <alexandre.dantas@yahoo.com.br>
2025-08-17 15:27:56 -0300

    using new ffmpegkit library

--- lib/src/nyx_converter/nyx_ff_converter.dart
+++ lib/src/nyx_converter/nyx_ff_converter.dart
@@ -1,10 +1,10 @@
 import 'dart:developer';
 
-import 'package:ffmpeg_kit_flutter_full_gpl/ffmpeg_kit.dart';
-import 'package:ffmpeg_kit_flutter_full_gpl/log.dart';
-import 'package:ffmpeg_kit_flutter_full_gpl/return_code.dart';
-import 'package:ffmpeg_kit_flutter_full_gpl/session.dart';
-import 'package:ffmpeg_kit_flutter_full_gpl/session_state.dart';
+import 'package:ffmpeg_kit_flutter_new/ffmpeg_kit.dart';
+import 'package:ffmpeg_kit_flutter_new/log.dart';
+import 'package:ffmpeg_kit_flutter_new/return_code.dart';
+import 'package:ffmpeg_kit_flutter_new/session.dart';
+import 'package:ffmpeg_kit_flutter_new/session_state.dart';
 import 'package:nyx_converter/nyx_converter.dart';
 
 class NyxFFConverter {
